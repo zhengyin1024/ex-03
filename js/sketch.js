382d8ae90c7bd76cc0d05cf6b8c56c790524089d
zhengyin <zl2522@columbia.edu>
2017-03-30 23:29:29 -0400

    First Commit

---
+++ js/sketch.js
@@ -0,0 +1,17 @@
+var citibikeData;
+
+var apiKey  = 'eaf69ef47b772f01998d2ccb237bb48f';
+
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var city;
+var units   = 'metric';
+
+var weatherData;
+
+var temperature = 0;
+var humidity = 0;
+var cloud =0;
+
+var button;
+
+var dimensionX = 50;
